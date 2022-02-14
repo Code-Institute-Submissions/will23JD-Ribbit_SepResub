b2a9dfb975c8a286dba7f823a3630bfd69ef314a
will23JD <will_jd@outlook.com>
2022-02-14 20:22:57 +0000

    Add views, templates and urls

---
+++ discussion/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    path('', views.DiscussionList.as_view(), name='home')
+]
--- discussion/views.py
+++ discussion/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import discussion
 
-# Create your views here.
+class DiscussionList(generic.ListView):
+    model = discussion
+    template_name = 'index.html'
+    paginate_by = 6
