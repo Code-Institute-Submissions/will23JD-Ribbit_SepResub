f6a2a023c06c33c073bf3b0e07969e30d56d246b
will23JD <will_jd@outlook.com>
2022-02-14 19:24:28 +0000

    Add discussion model class

--- discussion/models.py
+++ discussion/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+# Discussion model
+class discussion(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="ribbit_discussion")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name='discussion_likes', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
