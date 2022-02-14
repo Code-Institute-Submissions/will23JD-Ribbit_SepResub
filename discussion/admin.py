fc9d31c243e2037f36c3ad25ec0a3d6de22fe9da
will23JD <will_jd@outlook.com>
2022-02-14 19:48:55 +0000

    Add superUser and create discussions

--- discussion/admin.py
+++ discussion/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import discussion
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(discussion)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('created_on', 'author')
+    list_display = ('title', 'author', 'created_on')
+    search_fields = ['title', 'content']
+    summernote_fields = ('content')
