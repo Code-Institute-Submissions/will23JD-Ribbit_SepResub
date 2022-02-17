26a71f287664a1acd299e4690cc71fe45585aacd
will23JD <will_jd@outlook.com>
2022-02-17 17:37:24 +0000

    Add Link to open a discussion

--- discussion/urls.py
+++ discussion/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.DiscussionList.as_view(), name='home')
+    path('', views.DiscussionList.as_view(), name='home'),
+    path('<slug:slug>/', views.DiscussionOpen.as_view(), name='discussionOpen'),
 ]
--- discussion/views.py
+++ discussion/views.py
@@ -6,3 +6,8 @@ class DiscussionList(generic.ListView):
     model = discussion
     template_name = 'index.html'
     paginate_by = 9
+
+
+class DiscussionOpen(generic.DetailView):
+    model = discussion
+    template_name = 'discussion_open.html'
