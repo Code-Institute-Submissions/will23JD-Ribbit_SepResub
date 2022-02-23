eaabddab900e7963e93d4bcd3ca456e1dd694e5a
will23JD <will_jd@outlook.com>
2022-02-23 21:54:36 +0000

    Add auto fill author

--- discussion/forms.py
+++ discussion/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 from .models import discussion
 
+
 class DiscussionForm(forms.ModelForm):
     class Meta:
         model = discussion
         fields = ('title', 'slug', 'author', 'featured_image', 'excerpt', 'content')
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'slug': forms.TextInput(attrs={'class': 'form-control', 'slug': ('title',)}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'type': 'hidden', 'value': '', 'id': 'author'}),
             'featured_image': forms.FileInput(attrs={'class': 'form-control'}),
             'excerpt': forms.Textarea(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
